9b84fbd6032911c7941a4f767683633c93f5b18f
ReadySetGet <adizar135@gmail.com>
2025-07-04 19:42:51 +0200

    Documentation for sprint-1 functionalities added

---
+++ app/models/__init__.py
@@ -0,0 +1,10 @@
+"""Package containing data models for the fuzzy inference systems (fis) used.
+
+Modules:
+
+    fis_model.py: main fis data model with means of its edition
+
+Subpackages:
+
+    modelsresources: necessary extensions of the 'fuzzylab' library
+"""
