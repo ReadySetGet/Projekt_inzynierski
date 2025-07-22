969eadbaeb55739310815082bdca304426fa3f1a
ReadySetGet <adizar135@gmail.com>
2025-07-22 14:45:22 +0200

    Added importing FIS system to model from .fis file
    Closes FIS-152

--- app/models/fis_model.py
+++ app/models/fis_model.py
@@ -4,7 +4,6 @@
 Classes:
 
     FISModel: data model class for a fis system
-
 """
 import fuzzylab as fl
 from fuzzylab.FuzzyInferenceSystem import FuzzyInferenceSystem
@@ -102,19 +101,29 @@ class FISModel:
             Update a given rule.
     """
 
-    _fis: FuzzyInferenceSystem
+    _fis: fl.mamfis | fl.sugfis
     """The contained fis system."""
 
-    def __init__(self, fis: FuzzyInferenceSystem = None):
-        """Initialize a new class instance.
+    def __init__(self, fis: fl.mamfis | fl.sugfis = None,
+                 fis_name: str = "fis", fis_type: str = None):
+        """Initialize a new class instance, with the given fis system. If
+        "fis_type" is provided, a new 2-input-1-output (with 3 mfs each) fis of
+        the given type will be initiated.
 
         Parameters:
 
             fis (FuzzyInferenceSystem): The fis system to be used. If None,
                 a new Mamdani system will be generated.
+            fis_name (str): Name of the fis system.
+            fis_type (str): Type of the new fis system, "mamdani" or "sugeno".
         """
-        if fis is None:
-            self._fis = fl.mamfis("fis")
+        if fis_type is not None:
+            if fis_type == "sugeno":
+                self._fis = fl.sugfis(fis_name)
+            if fis_type == "mamdani":
+                self._fis = fl.mamfis(fis_name)
+        elif fis is None:
+            self._fis = fl.mamfis(fis_name)
         else:
             self._fis = fis
 
