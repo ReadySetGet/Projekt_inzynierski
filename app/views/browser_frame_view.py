4fa60ff98888473155a9072880be52f41cfe516c
Filip Wesołowski <szark0702@gmail.com>
2025-08-15 17:49:23 +0200

    refactoring names

--- app/views/browser_frame_view.py
+++ app/views/browser_frame_view.py
@@ -1,31 +1,33 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
 
+
 class BrowserFrameWidget(QtWidgets.QFrame):
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setObjectName("browserFrane")
         self._setup_ui()
+
     def _setup_ui(self):
         self.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
         self.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
         self.setObjectName("browserFrame")
 
-        self.systemBrowserTreeView = QtWidgets.QTreeView(parent=self)
-        self.systemBrowserTreeView.setGeometry(QtCore.QRect(0, 300, 301, 341))
-        self.systemBrowserTreeView.setObjectName("systemBrowserTreeView")
+        self.system_browser_tree_view = QtWidgets.QTreeView(parent=self)
+        self.system_browser_tree_view.setGeometry(QtCore.QRect(0, 300, 301, 341))
+        self.system_browser_tree_view.setObjectName("system_browser_tree_view")
 
-        self.systemBrowserLabel = QtWidgets.QLabel(parent=self)
-        self.systemBrowserLabel.setGeometry(QtCore.QRect(4, 274, 281, 21))
-        self.systemBrowserLabel.setObjectName("systemBrowserLabel")
+        self.system_browser_label = QtWidgets.QLabel(parent=self)
+        self.system_browser_label.setGeometry(QtCore.QRect(4, 274, 281, 21))
+        self.system_browser_label.setObjectName("system_browser_label")
 
-        self.designBrowserLabel = QtWidgets.QLabel(parent=self)
-        self.designBrowserLabel.setGeometry(QtCore.QRect(10, 10, 111, 16))
-        self.designBrowserLabel.setObjectName("designBrowserLabel")
+        self.design_browser_label = QtWidgets.QLabel(parent=self)
+        self.design_browser_label.setGeometry(QtCore.QRect(10, 10, 111, 16))
+        self.design_browser_label.setObjectName("design_browser_label")
 
         self._retranslate_ui()
 
     def _retranslate_ui(self):
         _translate = QtCore.QCoreApplication.translate
-        self.systemBrowserLabel.setText(_translate("MainWindow", "SYSTEM BROWSER"))
-        self.designBrowserLabel.setText(_translate("MainWindow", "DESIGN BROWSER"))
+        self.system_browser_label.setText(_translate("MainWindow", "SYSTEM BROWSER"))
+        self.design_browser_label.setText(_translate("MainWindow", "DESIGN BROWSER"))
--- app/views/top_menu_view.py
+++ app/views/top_menu_view.py
@@ -1,50 +1,51 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
 
+
 class TopMenu(QtWidgets.QTabWidget):
-    addInputClicked = QtCore.pyqtSignal()
-    deleteInputClicked = QtCore.pyqtSignal()
-    addOutputClicked = QtCore.pyqtSignal()
-    deleteOutputClicked = QtCore.pyqtSignal()
+    add_input_clicked = QtCore.pyqtSignal()
+    delete_input_clicked = QtCore.pyqtSignal()
+    add_output_clicked = QtCore.pyqtSignal()
+    delete_output_clicked = QtCore.pyqtSignal()
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setObjectName("topMenu")
         self._setup_ui()
 
     def _setup_ui(self):
-
         self.designTab = QtWidgets.QWidget()
         self.designTab.setObjectName("designTab")
 
-        self.inputOutputButtonArea = QtWidgets.QScrollArea(parent=self.designTab)
-        self.inputOutputButtonArea.setGeometry(QtCore.QRect(180, 0, 221, 131))
-        self.inputOutputButtonArea.setWidgetResizable(True)
-        self.inputOutputButtonArea.setObjectName("inputOutputButtonArea")
+        self.input_output_button_area = QtWidgets.QScrollArea(parent=self.designTab)
+        self.input_output_button_area.setGeometry(QtCore.QRect(180, 0, 221, 131))
+        self.input_output_button_area.setWidgetResizable(True)
+        self.input_output_button_area.setObjectName("input_output_button_area")
 
-        self.scrollAreaWidgetContents_3 = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents_3.setGeometry(QtCore.QRect(0, 0, 219, 129))
-        self.scrollAreaWidgetContents_3.setObjectName("scrollAreaWidgetContents_3")
+        self.scroll_area_widget_contents_3 = QtWidgets.QWidget()
+        self.scroll_area_widget_contents_3.setGeometry(QtCore.QRect(0, 0, 219, 129))
+        self.scroll_area_widget_contents_3.setObjectName("scroll_area_widget_contents_3")
 
-        self.addInputButton = QtWidgets.QPushButton(parent=self.scrollAreaWidgetContents_3)
-        self.addInputButton.setGeometry(QtCore.QRect(10, 10, 91, 41))
-        self.addInputButton.setObjectName("addInputButton")
-        self.addInputButton.clicked.connect(self.addInputClicked.emit)
+        self.add_input_button = QtWidgets.QPushButton(parent=self.scroll_area_widget_contents_3)
+        self.add_input_button.setGeometry(QtCore.QRect(10, 10, 91, 41))
+        self.add_input_button.setObjectName("add_input_button")
+        self.add_input_button.clicked.connect(self.add_input_clicked.emit)
 
-        self.deleteInputButton = QtWidgets.QPushButton(parent=self.scrollAreaWidgetContents_3)
-        self.deleteInputButton.setGeometry(QtCore.QRect(110, 10, 91, 41))
-        self.deleteInputButton.setObjectName("deleteInputButton")
-        self.deleteInputButton.clicked.connect(self.deleteInputClicked.emit)
+        self.delete_input_button = QtWidgets.QPushButton(parent=self.scroll_area_widget_contents_3)
+        self.delete_input_button.setGeometry(QtCore.QRect(110, 10, 91, 41))
+        self.delete_input_button.setObjectName("delete_input_button")
+        self.delete_input_button.clicked.connect(self.delete_input_clicked.emit)
 
-        self.addOutputButton = QtWidgets.QPushButton(parent=self.scrollAreaWidgetContents_3)
-        self.addOutputButton.setGeometry(QtCore.QRect(10, 70, 91, 41))
-        self.addOutputButton.setObjectName("addOutputButton")
-        self.addOutputButton.clicked.connect(self.addOutputClicked.emit)
+        self.add_output_button = QtWidgets.QPushButton(parent=self.scroll_area_widget_contents_3)
+        self.add_output_button.setGeometry(QtCore.QRect(10, 70, 91, 41))
+        self.add_output_button.setObjectName("add_output_button")
+        self.add_output_button.clicked.connect(self.add_output_clicked.emit)
 
-        self.deleteOutputButton = QtWidgets.QPushButton(parent=self.scrollAreaWidgetContents_3)
-        self.deleteOutputButton.setGeometry(QtCore.QRect(110, 70, 91, 41))
-        self.deleteOutputButton.setObjectName("deleteOutputButton")
-        self.deleteOutputButton.clicked.connect(self.deleteOutputClicked.emit)
+        self.delete_output_button = QtWidgets.QPushButton(parent=self.scroll_area_widget_contents_3)
+        self.delete_output_button.setGeometry(QtCore.QRect(110, 70, 91, 41))
+        self.delete_output_button.setObjectName("delete_output_button")
+        self.delete_output_button.clicked.connect(self.delete_output_clicked.emit)
 
-        self.inputOutputButtonArea.setWidget(self.scrollAreaWidgetContents_3)
+        self.input_output_button_area.setWidget(self.scroll_area_widget_contents_3)
 
         self.addTab(self.designTab, "")
         self.tuningTab = QtWidgets.QWidget()
@@ -55,9 +56,9 @@ class TopMenu(QtWidgets.QTabWidget):
 
     def _retranslate_ui(self):
         _translate = QtCore.QCoreApplication.translate
-        self.addInputButton.setText(_translate("MainWindow", "Add Input"))
-        self.deleteInputButton.setText(_translate("MainWindow", "Delete Input"))
-        self.addOutputButton.setText(_translate("MainWindow", "Add Output"))
-        self.deleteOutputButton.setText(_translate("MainWindow", "Delete Output"))
+        self.add_input_button.setText(_translate("MainWindow", "Add Input"))
+        self.delete_input_button.setText(_translate("MainWindow", "Delete Input"))
+        self.add_output_button.setText(_translate("MainWindow", "Add Output"))
+        self.delete_output_button.setText(_translate("MainWindow", "Delete Output"))
         self.setTabText(self.indexOf(self.designTab), _translate("MainWindow", "Design"))
         self.setTabText(self.indexOf(self.tuningTab), _translate("MainWindow", "Tuning"))
