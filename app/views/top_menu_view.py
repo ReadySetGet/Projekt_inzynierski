c22d23aa86564ec2b1f0a7f3a7e77db9e91f08e3
Filip Wesołowski <szark0702@gmail.com>
2025-09-20 20:12:22 +0200

    Added import button

--- app/views/top_menu_view.py
+++ app/views/top_menu_view.py
@@ -2,18 +2,7 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 
 
 class TopMenu(QtWidgets.QTabWidget):
-    add_input_clicked = QtCore.pyqtSignal()
-    delete_input_clicked = QtCore.pyqtSignal()
-    add_output_clicked = QtCore.pyqtSignal()
-    delete_output_clicked = QtCore.pyqtSignal()
-    help_clicked = QtCore.pyqtSignal()
-    settings_clicked = QtCore.pyqtSignal()
-    conversion_clicked = QtCore.pyqtSignal()
-    new_clicked = QtCore.pyqtSignal()
     import_clicked = QtCore.pyqtSignal()
-    export_clicked = QtCore.pyqtSignal()
-
-    system_type = 'Mamdani'
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -25,6 +14,22 @@ class TopMenu(QtWidgets.QTabWidget):
         self.designTab = QtWidgets.QWidget()
         self.designTab.setObjectName("designTab")
 
+        self.files_management_button_area = QtWidgets.QScrollArea(parent=self.designTab)
+        self.files_management_button_area.setGeometry(QtCore.QRect(0, 0, 271, 131))
+        self.files_management_button_area.setWidgetResizable(True)
+        self.files_management_button_area.setObjectName("files_management_button_area")
+
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 269, 129))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+
+        self.import_button = QtWidgets.QToolButton(parent=self.scrollAreaWidgetContents)
+        self.import_button.setGeometry(QtCore.QRect(100, 40, 71, 41))
+        self.import_button.setObjectName("import_button")
+        self.import_button.clicked.connect(self._import_button_clicked)
+
+        self.files_management_button_area.setWidget(self.scrollAreaWidgetContents)
+
         self.addTab(self.designTab, "")
         self.tuningTab = QtWidgets.QWidget()
         self.tuningTab.setObjectName("tuningTab")
@@ -32,5 +37,9 @@ class TopMenu(QtWidgets.QTabWidget):
 
     def _retranslate_ui(self):
         _translate = QtCore.QCoreApplication.translate
+        self.import_button.setText(_translate("MainWindow", "Import"))
         self.setTabText(self.indexOf(self.designTab), _translate("MainWindow", "Design"))
         self.setTabText(self.indexOf(self.tuningTab), _translate("MainWindow", "Tuning"))
+
+    def _import_button_clicked(self):
+        self.import_clicked.emit()
