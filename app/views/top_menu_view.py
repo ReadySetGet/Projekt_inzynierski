254e436787e6a1788c76afb56aea69c9f5317ecf
Filip Wesołowski <szark0702@gmail.com>
2025-09-20 18:46:17 +0200

    Added 'Add Output' and 'Delete Output' buttons to the ui

--- app/views/top_menu_view.py
+++ app/views/top_menu_view.py
@@ -2,18 +2,8 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 
 
 class TopMenu(QtWidgets.QTabWidget):
-    add_input_clicked = QtCore.pyqtSignal()
-    delete_input_clicked = QtCore.pyqtSignal()
     add_output_clicked = QtCore.pyqtSignal()
     delete_output_clicked = QtCore.pyqtSignal()
-    help_clicked = QtCore.pyqtSignal()
-    settings_clicked = QtCore.pyqtSignal()
-    conversion_clicked = QtCore.pyqtSignal()
-    new_clicked = QtCore.pyqtSignal()
-    import_clicked = QtCore.pyqtSignal()
-    export_clicked = QtCore.pyqtSignal()
-
-    system_type = 'Mamdani'
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -25,6 +15,27 @@ class TopMenu(QtWidgets.QTabWidget):
         self.designTab = QtWidgets.QWidget()
         self.designTab.setObjectName("designTab")
 
+        self.input_output_button_area = QtWidgets.QScrollArea(parent=self.designTab)
+        self.input_output_button_area.setGeometry(QtCore.QRect(270, 0, 221, 131))
+        self.input_output_button_area.setWidgetResizable(True)
+        self.input_output_button_area.setObjectName("input_output_button_area")
+
+        self.scroll_area_widget_contents_3 = QtWidgets.QWidget()
+        self.scroll_area_widget_contents_3.setGeometry(QtCore.QRect(0, 0, 219, 129))
+        self.scroll_area_widget_contents_3.setObjectName("scroll_area_widget_contents_3")
+
+        self.add_output_button = QtWidgets.QPushButton(parent=self.scroll_area_widget_contents_3)
+        self.add_output_button.setGeometry(QtCore.QRect(10, 70, 91, 41))
+        self.add_output_button.setObjectName("add_output_button")
+        self.add_output_button.clicked.connect(self._add_output_button_clicked)
+
+        self.delete_output_button = QtWidgets.QPushButton(parent=self.scroll_area_widget_contents_3)
+        self.delete_output_button.setGeometry(QtCore.QRect(110, 70, 91, 41))
+        self.delete_output_button.setObjectName("delete_output_button")
+        self.delete_output_button.clicked.connect(self._del_output_button_clicked)
+
+        self.input_output_button_area.setWidget(self.scroll_area_widget_contents_3)
+
         self.addTab(self.designTab, "")
         self.tuningTab = QtWidgets.QWidget()
         self.tuningTab.setObjectName("tuningTab")
@@ -32,5 +43,13 @@ class TopMenu(QtWidgets.QTabWidget):
 
     def _retranslate_ui(self):
         _translate = QtCore.QCoreApplication.translate
+        self.add_output_button.setText(_translate("MainWindow", "Add Output"))
+        self.delete_output_button.setText(_translate("MainWindow", "Delete Output"))
         self.setTabText(self.indexOf(self.designTab), _translate("MainWindow", "Design"))
         self.setTabText(self.indexOf(self.tuningTab), _translate("MainWindow", "Tuning"))
+
+    def _add_output_button_clicked(self):
+        self.add_output_clicked.emit()
+
+    def _del_output_button_clicked(self):
+        self.delete_output_clicked.emit()
