a34187a4ba995d40217301489f94bbb848eba9ed
Filip Wesołowski <szark0702@gmail.com>
2025-09-20 20:21:01 +0200

    Deleted unneeded variable

--- app/views/top_menu_view.py
+++ app/views/top_menu_view.py
@@ -2,18 +2,7 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 
 
 class TopMenu(QtWidgets.QTabWidget):
-    add_input_clicked = QtCore.pyqtSignal()
-    delete_input_clicked = QtCore.pyqtSignal()
-    add_output_clicked = QtCore.pyqtSignal()
-    delete_output_clicked = QtCore.pyqtSignal()
-    help_clicked = QtCore.pyqtSignal()
-    settings_clicked = QtCore.pyqtSignal()
-    conversion_clicked = QtCore.pyqtSignal()
-    new_clicked = QtCore.pyqtSignal()
-    import_clicked = QtCore.pyqtSignal()
     export_clicked = QtCore.pyqtSignal()
-
-    system_type = 'Mamdani'
 
     def __init__(self, parent=None):
         super().__init__(parent)
