039d4252dab6a87b29c9351db6e3fd27ffdbfbbf
Filip Wesołowski <szark0702@gmail.com>
2025-09-20 20:18:06 +0200

    Added export button

--- app/views/top_menu_view.py
+++ app/views/top_menu_view.py
@@ -25,6 +25,22 @@ class TopMenu(QtWidgets.QTabWidget):
         self.designTab = QtWidgets.QWidget()
         self.designTab.setObjectName("designTab")
 
+        self.files_management_button_area = QtWidgets.QScrollArea(parent=self.designTab)
+        self.files_management_button_area.setGeometry(QtCore.QRect(0, 0, 271, 131))
+        self.files_management_button_area.setWidgetResizable(True)
+        self.files_management_button_area.setObjectName("files_management_button_area")
+
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 269, 129))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+
+        self.export_button = QtWidgets.QToolButton(parent=self.scrollAreaWidgetContents)
+        self.export_button.setGeometry(QtCore.QRect(180, 41, 81, 41))
+        self.export_button.setObjectName("export_button")
+        self.export_button.clicked.connect(self._export_button_clicked)
+
+        self.files_management_button_area.setWidget(self.scrollAreaWidgetContents)
+
         self.addTab(self.designTab, "")
         self.tuningTab = QtWidgets.QWidget()
         self.tuningTab.setObjectName("tuningTab")
@@ -33,4 +49,8 @@ class TopMenu(QtWidgets.QTabWidget):
     def _retranslate_ui(self):
         _translate = QtCore.QCoreApplication.translate
         self.setTabText(self.indexOf(self.designTab), _translate("MainWindow", "Design"))
+        self.export_button.setText(_translate("MainWindow", "Export"))
         self.setTabText(self.indexOf(self.tuningTab), _translate("MainWindow", "Tuning"))
+
+    def _export_button_clicked(self):
+        self.export_clicked.emit()
