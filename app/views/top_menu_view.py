97e163c8e3f465d20689f6458eaabfaba0b60150
Filip Wesołowski <szark0702@gmail.com>
2025-09-21 19:49:30 +0200

    Added docstrings

--- app/views/top_menu_view.py
+++ app/views/top_menu_view.py
@@ -1,18 +1,39 @@
+"""Create a button area element GUI object above the central window of the application.
+
+    Classes:
+        TopMenu: button area element inheriting from QTabWidget.
+"""
 from PyQt6 import QtCore, QtGui, QtWidgets
 
 
 class TopMenu(QtWidgets.QTabWidget):
+    """Class inheriting from QTabWidget.
+        Allows user interaction via QPushButton and QToolButton GUI elements .
+
+        Methods:
+            __init__(parent): create an instance of TopMenu and bind it to the parent window.
+
+        Attributes:
+            conversion_clicked: pyqtSignal which gets emitted to back end when conversion_button is clicked.
+            system_type: stores data about current fis system type.
+    """
     conversion_clicked = QtCore.pyqtSignal()
 
     system_type = 'Mamdani'
 
     def __init__(self, parent=None):
+        """Initialize a new class instance.
+
+            Parameters:
+                parent: The parent widget, in this case main window, to which the widget will be attached.
+        """
         super().__init__(parent)
         self.setObjectName("topMenu")
         self._setup_ui()
         self._retranslate_ui()
 
     def _setup_ui(self):
+        """Set up all the GUI sub elements."""
         self.designTab = QtWidgets.QWidget()
         self.designTab.setObjectName("designTab")
 
@@ -27,6 +48,7 @@ class TopMenu(QtWidgets.QTabWidget):
         self.addTab(self.tuningTab, "")
 
     def _retranslate_ui(self):
+        """Add text to all the respective GUI elements."""
         _translate = QtCore.QCoreApplication.translate
         if self.system_type == 'Mamdani':
             self.conversion_button.setText(_translate("MainWindow", "Mamdani to Sugeno"))
@@ -38,7 +60,10 @@ class TopMenu(QtWidgets.QTabWidget):
         self.setTabText(self.indexOf(self.tuningTab), _translate("MainWindow", "Tuning"))
 
     def _conversion_button_clicked(self):
-        """Function responsible for converting system from mamdani to sugeno and vice versa"""
+        """Converts system from mamdani to sugeno and vice versa.
+        Updates the text on the button to indicate the change.
+        Emits a signal to the backend to convert the system.
+        """
         if self.system_type == "Mamdani":
             self.system_type = "Sugeno"
             self.conversion_button.setText("Sugeno to Mamdani")
