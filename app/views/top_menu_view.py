aea46d7f7af7fc3314ed260253c37d39f0a8290d
Filip Wesołowski <szark0702@gmail.com>
2025-09-21 19:17:45 +0200

    Added docstrings

--- app/views/top_menu_view.py
+++ app/views/top_menu_view.py
@@ -1,16 +1,36 @@
+"""Create a button area element GUI object above the central window of the application.
+
+    Classes:
+        TopMenu: button area element inheriting from QTabWidget.
+"""
 from PyQt6 import QtCore, QtGui, QtWidgets
 
 
 class TopMenu(QtWidgets.QTabWidget):
+    """Class inheriting from QTabWidget.
+        Allows user interaction via QPushButton and QToolButton GUI elements .
+
+        Methods:
+            __init__(parent): create an instance of TopMenu and bind it to the parent window.
+
+        Attributes:
+            import_clicked: pyqtSignal which gets emitted to back end when add_input_button is clicked.
+    """
     import_clicked = QtCore.pyqtSignal()
 
     def __init__(self, parent=None):
+        """Initialize a new class instance.
+
+            Parameters:
+                parent: The parent widget, in this case main window, to which the widget will be attached.
+        """
         super().__init__(parent)
         self.setObjectName("topMenu")
         self._setup_ui()
         self._retranslate_ui()
 
     def _setup_ui(self):
+        """Set up all the GUI sub elements."""
         self.designTab = QtWidgets.QWidget()
         self.designTab.setObjectName("designTab")
 
@@ -36,10 +56,12 @@ class TopMenu(QtWidgets.QTabWidget):
         self.addTab(self.tuningTab, "")
 
     def _retranslate_ui(self):
+        """Add text to all the respective GUI elements."""
         _translate = QtCore.QCoreApplication.translate
         self.import_button.setText(_translate("MainWindow", "Import"))
         self.setTabText(self.indexOf(self.designTab), _translate("MainWindow", "Design"))
         self.setTabText(self.indexOf(self.tuningTab), _translate("MainWindow", "Tuning"))
 
     def _import_button_clicked(self):
+        """Emit a signal that new data is supposed to get imported."""
         self.import_clicked.emit()
